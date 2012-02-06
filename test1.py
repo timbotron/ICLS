e792397a4e55bb10188a6bbc23857cc4a800d097
Tim Habersack <tim@hithlonde.com>
2012-02-06 12:57:05 -0800

    Played around with color messages, worked on arguments a little.

--- test1.py
+++ test1.py
@@ -1,4 +1,12 @@
 import sys
 
-for arg in sys.argv:
-    print arg
+#for arg in sys.argv:
+#    print arg
+
+#print sys.argv[1]
+results=''
+
+if sys.argv[1]=='-c':
+	results+='\033[1;32mCompleted a task, way to go!\033[1;m\n'
+results+='\033[1;36mLog Entry Submitted Successfully\033[1;m'
+print results
