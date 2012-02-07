a6059250d037d44b3930b87349eb1592dffc2671
Tim Habersack <tim@hithlonde.com>
2012-02-06 16:04:05 -0800

    made colorize(), started putting in logic for prettifying output, and seperating out the entry.

--- test1.py
+++ test1.py
@@ -1,12 +1,25 @@
 import sys
 
-#for arg in sys.argv:
-#    print arg
+def colorize(the_color='blue',entry='',new_line=0):
+	color={'gray':30,'green':32,'red':31,'blue':34,'magenta':35,'cyan':36}
+	if new_line==1:
+		new_line='\n'
+	else:
+		new_line=''
+	return_me='\033[1;'+str(color[the_color])+'m'+entry+'\033[1;m'+new_line
+	return return_me
 
-#print sys.argv[1]
-results=''
+
+entry_text=''
+output=''
 
 if sys.argv[1]=='-c':
-	results+='\033[1;32mCompleted a task, way to go!\033[1;m\n'
-results+='\033[1;36mLog Entry Submitted Successfully\033[1;m'
-print results
+	output+=colorize('cyan','Completed a task, way to go!',1)
+	entry_text=sys.argv[2].replace('\\','')
+else:
+	entry_text=sys.argv[1].replace('\\','')
+
+output+=colorize('gray','Entry: '+entry_text,1)
+output+=colorize('green','Log entry submitted successfully.')
+print output
+
