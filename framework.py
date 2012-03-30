991a4ea0f01a90ab3d8a27fad4c31df226ed2bc0
Tim Habersack <tim@hithlonde.com>
2012-03-30 16:57:36 -0700

    Added new license, also finished documentation.

--- framework.py
+++ framework.py
@@ -1,3 +1,19 @@
+#	This file is part of ICLS.
+#
+#	ICLS is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	ICLS is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with ICLS.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import xml.dom.minidom
 from time import strftime, strptime
 from sys import exit
--- parsing.py
+++ parsing.py
@@ -1,7 +1,22 @@
+#	This file is part of ICLS.
+#
+#	ICLS is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	ICLS is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with ICLS.  If not, see <http://www.gnu.org/licenses/>.
+
 from time import strftime, localtime
 from uuid import uuid4
 from re import compile
-from framework import aws_print_error, colorize, print_help, makeover
+from framework import aws_print_error, colorize, print_help, makeover, print_error
 from boto.exception import SDBResponseError
 import config
 
