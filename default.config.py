456c6998e6eff7069cc6186569c8e59eac9eb178
Tim Habersack <tim@hithlonde.com>
2012-04-14 16:39:06 -0700

    updated default.config.py

--- default.config.py
+++ default.config.py
@@ -1,4 +1,5 @@
 values={'access_key_id':'access key here',
-		'secret_access_key':'secret key here',
-		'domain':'your desired domain name here'
-		'default':'your default tag here'}
+                'secret_access_key':'secret key here',
+                'domain':'your desired domain name here'
+                'default':'your default tag here',
+                'region':'us-west-2'}
